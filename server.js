072b43b1de3f1c613fd46fae97bf7a6d61886ac9
BAM-hub <bsharamin12@gmail.com>
2022-03-20 01:41:53 +0300

    the socket http server with basic functionality

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const { userJoin, userLeave, getMarkerCords } = require('./utils/users');
+
+const app = express();
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer);
+
+const TYPES = ['captin', 'passenger'];
+
+
+io.on('connection', socket => {
+  console.log(socket.id);
+
+  socket.on('join', user => {
+    console.log(user);
+    socket.join(user.type);
+    userJoin(user);
+    const markerCords = getMarkerCords(user.type);
+    socket.emit('initial-cords', markerCords);
+  });
+
+  socket.on('location-update', (id, cords, type) => {
+    const localType = TYPES.filter(TYPE => type !== TYPE);
+    socket.to(localType).emit('cords-update', ({id, cords}));
+  });
+
+  socket.on('disconnect', user => {
+    userLeave(user.id, user.type);
+    const type = TYPES.filter(TYPE => user.type !== TYPE);
+    socket.to(type[0]).emit('user-left', user.id);
+    console.log('disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+httpServer.listen(PORT, () => {
+  console.log(`Server started on ${PORT}`);
+});
