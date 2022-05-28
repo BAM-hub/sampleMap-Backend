3736c59ae21e4afdbe04cc52949840a9989b6653
BAM-hub <bsharamin12@gmail.com>
2022-05-28 22:37:00 +0300

    migrated to node

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { createServer } = require('http');
 const { Server } = require('socket.io');
-const { userJoin, userLeave, getMarkerCords } = require('./utils/users');
+const { userJoin, userLeave, getMarkerCords, inLine } = require('./utils/users');
 
 const app = express();
 
@@ -14,24 +14,41 @@ io.on('connection', socket => {
   console.log(socket.id);
 
   socket.on('join', user => {
-    socket.join(user.type);
+    // console.log('id', user.id)
+    socket.join(user.id);
     userJoin(user);
     const markerCords = getMarkerCords(user.type);
     console.log('cords', markerCords);
-    socket.emit('initial-coords', markerCords);
+    socket.emit('initial-coords', {id: user.id, markerCords});
   });
 
-  // socket.on('location-update', (id, cords, type) => {
-  //   const localType = TYPES.filter(TYPE => type !== TYPE);
-  //   socket.to(localType).emit('cords-update', ({id, cords}));
-  // });
+  socket.on('leave', user => socket.leave(user.id));
 
-  // socket.on('disconnect', user => {
-  //   userLeave(user.id, user.type);
-  //   const type = TYPES.filter(TYPE => user.type !== TYPE);
-  //   socket.to(type[0]).emit('user-left', user.id);
-  //   console.log('disconnected');
-  // });
+  socket.on('driverBroadcast', user => {
+    socket.emit('changeLocation', user);
+  });
+
+  socket.on('raidRequest', request => {
+    console.log(request)
+    let answer = inLine(request.location);
+    console.log(answer)
+    if(answer)
+      return socket.to(request.driverId).emit('raidRequest', request);
+    socket.emit('rideResponse', { ...request, ...answer });
+  });
+
+  socket.on('answer', answer => {
+    socket.to(request.userId).emit('rideResponse', answer);
+  });
+
+  socket.on('cancelRide', ride => {
+    socket.to(ride.driverId).emit('cancelRide', ride);
+  });
+
+  socket.on('disconnect', user => {
+    userLeave(user.id, user.type);
+    socket.emit('user_disconnect', user);
+  });
 });
 
 const PORT = process.env.PORT || 5000;
--- utils/users.js
+++ utils/users.js
@@ -1,10 +1,56 @@
+const geolib = require('geolib');
+const { line } = require('../line.js');
+
+// let test = { latitude: 22.02159504627019, longitude: 25.71390630925344 };
+
 let captins = [
-  {"cords": {"latitude": 31.5219983, "longitude": -120.084}},
-  {"cords": {"latitude": 37.4219983, "longitude": -122.084}}
+  {
+    'sid': '0',
+    'id': '0',
+    'name': 'BAM',
+    'line': 'Amman As-Salt',
+    'distance': 'Calculating',
+    'time': 'Calculating',
+    'coords': {
+        'latitude': 31.966882,
+        'longitude': 35.988638
+    }
+},
+{
+    'sid': '1',
+    'id': '1',
+    'name': 'Ali',
+    'line': 'Amman As-Salt',
+    'distance': 'Calculating',
+    'time': 'Calculating',
+    'coords': {
+        'latitude': 31.963817,
+        'longitude': 35.975449
+    },
+
+  },
 ];
 let users = [
-  {"cords": {"latitude": 30.5219983, "longitude": -120.084}},
-  {"cords": {"latitude": 37.4219983, "longitude": -122.084}}
+  {
+    'sid': '2',
+    'id': '2',
+    'name': 'BAM',
+    'line': 'Amman As-Salt',
+    'coords': {
+            'latitude': 31.966882,
+            'longitude': 35.988638
+    },
+  },
+  {
+    'sid': '3',
+    'id': '3',
+    'name': 'Ali',
+    'line': 'Amman As-Salt',
+    'coords': {
+        'latitude': 31.963817,
+        'longitude': 35.975449
+    },
+  },
 ];
 
 function userJoin(user) {
@@ -17,13 +63,15 @@ function userLeave(id, type) {
   if(type === 'passenger') {
     const index = users.findIndex(user => user.id === id);
     if( index !== -1 ) {
-      return users.splice(index, 1)[0];
+      users = users.splice(index, 1)[0];
+      return users;
     }
   }
   
   const index = captins.findIndex(captin => captin.id === id);
   if( index !== -1 ) {
-    return captins.splice(index, 1)[0];
+    captins = captins.splice(index, 1)[0];
+    return captins;
   }
 };
 
@@ -34,8 +82,19 @@ function getMarkerCords(type) {
   return captins;
 }
 
+function inLine(coords) {
+  const answer = line.filter((l => geolib.getDistance(l, coords) < 100));
+  if(answer.length > 0)
+    return {
+      status: true
+    }
+  return {
+    status: false
+  }
+}
 module.exports = {
   userJoin,
   userLeave,
-  getMarkerCords
+  getMarkerCords,
+  inLine
 };
